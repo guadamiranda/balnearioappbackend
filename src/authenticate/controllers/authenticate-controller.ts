25efb7e0734feb83f8af731268847eccfd30e688
jonathan.calderon <jonathan.calderon@workiasolutions.com>
2024-01-05 19:34:24 -0300

    addAuthenticationValidate

--- src/authenticate/controllers/authenticate-controller.ts
+++ src/authenticate/controllers/authenticate-controller.ts
@@ -1,18 +1,22 @@
 
 import { AuthenticateServices } from '../services/authenticate-services';
 import { LoginRequest } from './dto/login-request';
+import { JwtService } from "@nestjs/jwt";
 import { 
     Controller,
     Post,
     HttpException,
     HttpStatus,
-    Body
+    Body,
+    Get,
+    Req
   } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthenticateController {
   constructor(
-    private readonly authService: AuthenticateServices
+    private readonly authService: AuthenticateServices,
+    private readonly jwtService: JwtService
   ) {}
 
   @Post('/login')
@@ -24,6 +28,18 @@ export class AuthenticateController {
       //TODO: Implementar manejo de excepciones
       const httpStatus = error.message = ('Invalid credentials') ? HttpStatus.UNAUTHORIZED :  HttpStatus.INTERNAL_SERVER_ERROR
       throw new HttpException(error.message, httpStatus)
+    }
+  }
+
+  @Get('/validate')
+  async validate(@Req() req): Promise<boolean> {
+    const token = req.headers.authorization.split(' ')[1]; // Assuming the token is passed in the Authorization header
+  
+    try {
+      await this.jwtService.verifyAsync(token);
+      return true; // Token is valid and not expired
+    } catch (error) {
+      return false; // Token is either invalid or expired
     }
   }
 }
